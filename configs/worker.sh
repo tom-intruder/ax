03270bd33970bc3fc9bf9359c174adfc97390655
Tom Steer <tom.steer@intruder.io>
2026-03-31 16:24:38 +0100

    Dynamic Configs v2

--- configs/worker.sh
+++ configs/worker.sh
@@ -3,9 +3,8 @@ set -e
 
 TASKS_PATH="/home/op/bin/tasks.py"
 
-source /home/op/.zshrc
 
-export $(xargs < /home/op/bin/worker.env)
+export $(/usr/bin/xargs < /home/op/bin/worker.env)
 
 if [ -z "$S3_CONFIG_BUCKET" ]; then
     echo "S3_CONFIG_BUCKET is not set" >&2
@@ -13,11 +12,11 @@ if [ -z "$S3_CONFIG_BUCKET" ]; then
 fi
 
 echo "Pulling latest tasks.py from s3://$S3_CONFIG_BUCKET/tasks.py"
-aws s3 cp "s3://$S3_CONFIG_BUCKET/tasks.py" "$TASKS_PATH"
+/usr/local/bin/aws s3 cp "s3://$S3_CONFIG_BUCKET/tasks.py" "$TASKS_PATH"
 
 echo "Pulling latest config from s3://$S3_CONFIG_BUCKET/config.env"
-aws s3 cp "s3://$S3_CONFIG_BUCKET/config.env" /home/op/bin/config.env
-export $(xargs < /home/op/bin/config.env)
+/usr/local/bin/aws s3 cp "s3://$S3_CONFIG_BUCKET/config.env" /home/op/bin/config.env
+export $(/usr/bin/xargs < /home/op/bin/config.env)
 
 echo "Starting Celery worker"
 exec /home/op/.local/bin/celery -A tasks worker --loglevel=info
